f13a9b2864c0841ac6b73c435c8cac359ac801aa
Jitan Gupta <gtangupta@gmail.com>
2020-11-11 22:40:08 +0530

    workspace setup

---
+++ src/app/core/data/people-id-like-to-meet.ts
@@ -0,0 +1,10 @@
+export const PeopleLikeToMeetList: PeopleLikeToMeet[] = [
+  { haveMet: false, name: 'John Papa', url: 'https://twitter.com/John_Papa' }
+];
+
+export class PeopleLikeToMeet {
+  haveMet: boolean;
+  name: string;
+  url: string;
+}
+
