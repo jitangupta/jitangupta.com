ec7152f8a64394562adff6f82879835c733b78c9
Jitan Gupta <gtangupta@gmail.com>
2021-02-21 12:27:33 +0530

    resume related update - development on hold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { ArticleComponent } from './pages/article/article.component';
 import { WeLoveStartupsComponent } from './pages/we-love-startups/we-love-startups.component';
 import { TechStuffComponent } from './pages/tech-stuff/tech-stuff.component';
 import { RuhanComponent } from './pages/ruhan/ruhan.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PeopleToMeetComponent, ArticleComponent, WeLoveStartupsComponent,
-    TechStuffComponent, RuhanComponent,
+    TechStuffComponent, RuhanComponent, ResumeComponent,
   ],
   imports: [
     BrowserModule,
